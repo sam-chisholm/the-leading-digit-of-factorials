fbfadacee4b44a48a01dc72e3931b3dda4c4b894
Rida Zouga <96395950+sam-chisholm@users.noreply.github.com>
2022-08-06 22:27:21 +0100

    Update introduction_to_sampling.py

--- introduction_to_sampling.py
+++ introduction_to_sampling.py
@@ -1,18 +1,8 @@
-# NOTE that this code takes on average a minute to finish execution
 import numpy as np
 
-# first, we'll store our population set in one single array
-# note that the first element of the array is 1 since 0! = 1
-population_data = np.array([1])
-
-factorial = 1
-for x in range(1, 10000):
-    factorial *= x
-
-    # after computing factorial we convert it into a string
-    # get that first element and converting that again into an integer
-    # then append that in our array
-    population_data = np.append(population_data, int(str(factorial)[0]))
+# first, we create 2 arrays to store our population
+leading_digit = np.arange(1, 10)
+relative_frequencies = [0.2957, 0.1788, 0.1276, 0.0963, 0.0794, 0.0715, 0.0571, 0.0510, 0.0426]
 
 # this line of code is made just to make sure that we get the same samples
 np.random.seed(0)
@@ -24,6 +14,6 @@ n = 10
 # here we'll be drawing 5 samples, each sample of size n
 # and for each sample, we're calculating some basic statistics
 for i in range(1, 6):
-    sample = np.random.choice(population_data, size=n, replace=True)
+    sample = np.random.choice(leading_digit, size=n, replace=True, p=relative_frequencies)
     print("sample " + str(i) + ":", sample, np.mean(sample),
           np.median(sample), np.var(sample, ddof=1), sep="\t\t")
